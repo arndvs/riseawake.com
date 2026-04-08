517e6e171c0ccb2a751d6f35471e2955d5654b6d
Aaron Davis <arndvs@gmail.com>
2026-04-07 22:19:28 -0700

    feat: rebrand blog post and rss feed from radiant to rise

--- src/app/blog/feed.xml/route.ts
+++ src/app/blog/feed.xml/route.ts
@@ -7,12 +7,12 @@ export async function GET(req: Request) {
   let siteUrl = new URL(req.url).origin
 
   let feed = new Feed({
-    title: 'The Radiant Blog',
+    title: 'RISE™ Blog',
     description:
-      'Stay informed with product updates, company news, and insights on how to sell smarter at your company.',
+      'Updates, observations, and communications from RISE™ Technologies.',
     author: {
-      name: 'Michael Foster',
-      email: 'michael.foster@example.com',
+      name: 'RISE™ Technologies',
+      email: 'press@riseco.online',
     },
     id: siteUrl,
     link: siteUrl,
