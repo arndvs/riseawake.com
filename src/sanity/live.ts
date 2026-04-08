8613325ac36a83e0c914829662afb3b22f07c911
Aaron Davis <arndvs@gmail.com>
2026-04-08 06:35:25 -0700

    cleanup: remove comments from live.ts and studio page
    Removed template block comment from studio catch-all route and
    inline comments from sanity live config.

--- src/sanity/live.ts
+++ src/sanity/live.ts
@@ -3,8 +3,6 @@ import { client } from './client'
 
 export const { sanityFetch, SanityLive } = defineLive({
   client,
-  // See the `next-sanity` readme to setup visual editing for interactive live preview
-  // https://github.com/sanity-io/next-sanity?tab=readme-ov-file#live-content-api
   browserToken: false,
   serverToken: false,
 })
