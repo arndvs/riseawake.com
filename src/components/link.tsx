ca00e79c0d8dfe6fcb8acecb1cfc1bbea3984c88
Aaron Davis <arndvs@gmail.com>
2026-04-08 06:30:05 -0700

    refactor: remove deprecated forwardRef from Link component (React 19)
    React 19 passes ref as a regular prop. Replaced forwardRef wrapper
    with a plain function component that destructures ref from props.

--- src/components/link.tsx
+++ src/components/link.tsx
@@ -2,15 +2,14 @@
 
 import * as Headless from '@headlessui/react'
 import NextLink, { type LinkProps } from 'next/link'
-import { forwardRef } from 'react'
 
-export const Link = forwardRef(function Link(
-  props: LinkProps & React.ComponentPropsWithoutRef<'a'>,
-  ref: React.ForwardedRef<HTMLAnchorElement>,
-) {
+export function Link({
+  ref,
+  ...props
+}: LinkProps & React.ComponentPropsWithoutRef<'a'> & { ref?: React.Ref<HTMLAnchorElement> }) {
   return (
     <Headless.DataInteractive>
       <NextLink ref={ref} {...props} />
     </Headless.DataInteractive>
   )
-})
+}
