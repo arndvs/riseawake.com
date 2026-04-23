528e849804871ba48b8540a9ea72c8f5573944ed
Aaron Davis <arndvs@gmail.com>
2026-04-23 16:21:12 -0700

    chore(sitemap): add noindex metadata to sitemap page

--- src/app/sitemap/page.tsx
+++ src/app/sitemap/page.tsx
@@ -1,8 +1,13 @@
 import { clsx } from 'clsx'
+import type { Metadata } from 'next'
 
 import { Footer } from '@/components/footer'
 import { Navbar } from '@/components/navbar'
 import Link from 'next/link'
+
+export const metadata: Metadata = {
+  robots: { index: false, follow: false },
+}
 
 const SECTIONS = [
   {
